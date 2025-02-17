8c61f1878cc6de50cfc660c4c5a8c43cd846f674
knh976 <khanh147.game@gmail.com>
2025-02-17 10:59:09 +0700

    chore: update test script

--- bin/test.sh
+++ bin/test.sh
@@ -11,6 +11,11 @@ echo -e "${GREEN}Running frontend unit tests ... ${NC}"
 sleep 2
 cd frontend && yarn test && cd ..
 
+echo -e "${GREEN}Preparing e2e...${NC}"
+cd backend && bundle exec rails s -p 3000 2>/dev/null &
+cd .. &
+sleep 5
+
 echo -e "${GREEN}Running frontend e2e tests ... ${NC}"
 sleep 2
 cd frontend && yarn e2e
